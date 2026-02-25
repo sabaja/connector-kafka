4022e1a2f73ac77f011b7a5adc67e1776f719c62
U0N9369 <jacopo.sabatini@esternibisp.com>
2026-02-25 16:21:06 +0100

    Imports

--- spring-kafka-producer/src/test/java/it/js/springkafkaproducer/SpringKafkaProducerApplicationTests.java
+++ spring-kafka-producer/src/test/java/it/js/springkafkaproducer/SpringKafkaProducerApplicationTests.java
@@ -3,9 +3,6 @@ package it.js.springkafkaproducer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
