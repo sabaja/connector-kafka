ae2edeb0ae180d93f6dcc2dbcec56a2b207bc183
U0N9369 <jacopo.sabatini@esternibisp.com>
2026-02-25 17:43:21 +0100

    Some changes in producer

--- spring-kafka-producer/src/main/java/it/js/springkafkaproducer/SpringKafkaProducerApplication.java
+++ spring-kafka-producer/src/main/java/it/js/springkafkaproducer/SpringKafkaProducerApplication.java
@@ -2,6 +2,7 @@ package it.js.springkafkaproducer;
 
 import it.js.commons.dto.NewOrder;
 import it.js.springkafkaproducer.service.KafkaProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import java.util.ArrayList;
 
 
+@Slf4j
 @SpringBootApplication
 public class SpringKafkaProducerApplication {
 
@@ -21,7 +23,8 @@ public class SpringKafkaProducerApplication {
     @Bean
     CommandLineRunner sender(KafkaProducer producer) {
 
-        return args -> {
+        return _ -> {
+            log.info("START INVIO");
             String message = "New order Patata";
             producer.send(message);
             final NewOrder newOrder = createNewOrder();
